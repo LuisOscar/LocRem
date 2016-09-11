8f7b846d87bb4a9ec74ca3a40bad16a15d52bb2c
Oscar Peredo <luisoscar@gmail.com>
2016-09-11 17:05:17 +0900

    Initial Commit

---
+++ LocationReminder WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  LocationReminder WatchKit Extension
+//
+//  Created by Luis Oscar Peredo on 9/11/16.
+//  Copyright © 2016 ShiroKuma. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
